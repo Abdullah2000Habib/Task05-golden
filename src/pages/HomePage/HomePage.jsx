9a1c6cfa92bcde52912ef91539a5ee93621f528d
Abdullah2000Habib <abd3habib@gmail.com>
2023-03-05 16:25:03 +0200

    Section4

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import foodSectionImg from "../../assets/images/about-image.png";
+import menuFood1 from "../../assets/images/menu/menu1.png";
+import menuFood10 from "../../assets/images/menu/menu10.png";
+import menuFood11 from "../../assets/images/menu/menu11.png";
+import menuFood12 from "../../assets/images/menu/menu12.png";
+import menuFood2 from "../../assets/images/menu/menu2.png";
+import menuFood3 from "../../assets/images/menu/menu3.png";
+import menuFood4 from "../../assets/images/menu/menu4.png";
+import menuFood5 from "../../assets/images/menu/menu5.png";
+import menuFood6 from "../../assets/images/menu/menu6.png";
+import menuFood7 from "../../assets/images/menu/menu7.png";
+import menuFood8 from "../../assets/images/menu/menu8.png";
+import menuFood9 from "../../assets/images/menu/menu9.png";
 import specialCardImage3 from "../../assets/images/special/Exotic-dishes.png";
 import specialCardImage1 from "../../assets/images/special/Fresh-food.png";
 import specialCardImage2 from "../../assets/images/special/skilled-Chef.png";
 import Button from '../../components/Button/Button';
 import Container from '../../components/Container/Container';
 import FoodSection from '../../components/FoodSection/FoodSection';
+import MenuItem from '../../components/MenuItem/MenuItem';
 import Navbar from '../../components/Navbar/Navbar';
 import SectionHeading from '../../components/SectionHeading/SectionHeading';
 import SpecialCard from '../../components/SpecialCard/SpecialCard';
@@ -31,12 +44,32 @@ render() {
                 <div className='section3Heading'>
                     <SectionHeading title='Special' description='What Makes Us Special' toggle={false}/>
                 </div>
-                
                 <div className='section3Cards'>
                     <SpecialCard img={specialCardImage1} title='Fresh Food' description='Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit. Sollicitudin Hendrerit A Amet Viverra. Nunc Pretium In Amet At Mattis Cras.'/>
                     <SpecialCard img={specialCardImage2} title='Skilled Chef' description='Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit. Sollicitudin Hendrerit A Amet Viverra. Nunc Pretium In Amet At Mattis Cras.'/>
                     <SpecialCard img={specialCardImage3} title='Exotic Dishes' description='Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit. Sollicitudin Hendrerit A Amet Viverra. Nunc Pretium In Amet At Mattis Cras.'/>
                 </div>
+            </div>
+            <div className='section4'>
+                <div className='section3Heading'>
+                        <SectionHeading title='Straight From Kitchen' description='Our Menu' toggle={false}/>
+                    </div>
+                <Container>
+                    <div className='section4Menus'>
+                        <MenuItem img={menuFood1} title='Lorem ipsum dolor sit amet' description='Lorem ipsum dolor sit amet' code='Rs 222'/>
+                        <MenuItem img={menuFood2} title='Lorem ipsum dolor sit amet' description='Lorem ipsum dolor sit amet' code='Rs 185'/>
+                        <MenuItem img={menuFood3} title='Lorem ipsum dolor sit amet' description='Lorem ipsum dolor sit amet' code='Rs 190'/>
+                        <MenuItem img={menuFood4} title='Lorem ipsum dolor sit amet' description='Lorem ipsum dolor sit amet' code='Rs 200'/>
+                        <MenuItem img={menuFood5} title='Lorem ipsum dolor sit amet' description='Lorem ipsum dolor sit amet' code='Rs 135'/>
+                        <MenuItem img={menuFood6} title='Lorem ipsum dolor sit amet' description='Lorem ipsum dolor sit amet' code='Rs 148'/>
+                        <MenuItem img={menuFood7} title='Lorem ipsum dolor sit amet' description='Lorem ipsum dolor sit amet' code='Rs 590'/>
+                        <MenuItem img={menuFood8} title='Lorem ipsum dolor sit amet' description='Lorem ipsum dolor sit amet' code='Rs 130'/>
+                        <MenuItem img={menuFood9} title='Lorem ipsum dolor sit amet' description='Lorem ipsum dolor sit amet' code='Rs 190'/>
+                        <MenuItem img={menuFood10} title='Lorem ipsum dolor sit amet' description='Lorem ipsum dolor sit amet' code='Rs 200'/>
+                        <MenuItem img={menuFood11} title='Lorem ipsum dolor sit amet' description='Lorem ipsum dolor sit amet' code='Rs 130'/>
+                        <MenuItem img={menuFood12} title='Lorem ipsum dolor sit amet' description='Lorem ipsum dolor sit amet' code='Rs 185'/>
+                    </div>
+                </Container>
             </div>
         </>
     )
