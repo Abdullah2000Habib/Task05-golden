42f0f8d00e7320276964789c170c938758d3e155
Abdullah2000Habib <abd3habib@gmail.com>
2023-03-05 15:20:09 +0200

    Section2

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import foodSectionImg from "../../assets/images/about-image.png";
 import Button from '../../components/Button/Button';
+import Container from '../../components/Container/Container';
+import FoodSection from '../../components/FoodSection/FoodSection';
 import Navbar from '../../components/Navbar/Navbar';
 import "./style.css";
 export default class HomePage extends Component {
@@ -13,6 +16,11 @@ render() {
                     <span className='section1Description'>Explore the authentic vegan dishes with your friends and family</span>
                     <Button title="Read more"/>
                 </div>
+            </div>
+            <div className='section2'>
+                <Container>
+                    <FoodSection title='About Us' description='Discover The Taste Of Worldclass Vegan Dishes From The Kitchen Of Go Green Dine'  body='Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit. Sollicitudin Hendrerit A Amet Viverra. Nunc Pretium In Amet At Mattis Cras. Quisque Commodo Mattis Ornare A Nec Odio. Pulvinar Orci, Egestas Eget Nulla Duis At Iaculis Nunc, Elit. Nibh Auctor Purus Nunc Tellus Pellentesque Nibh Mattis. Malesuada Integer Consectetur. Imperdiet Aliquam Quam Mauris Semper Suscipit. Molestie Maecenas Interdum Pharetra Id Velit Sed Nec.Tetur Sit Sagittis Pretium Eget Vitae Semper Pellentesque Pellentesque.' btnName='Read more' img={foodSectionImg}/>
+                </Container>
             </div>
         </>
     )
